50915fbbe9fac75d997a7c75453286be2501fc06
LoloKrikon <loloricoruizz12@gmail.com>
2026-03-10 13:32:27 +0100

    Corregir WebXR AR en Needle: desactivar Meta Quest y cargar modelo desde HTML

--- PagNeedle/ar-mindar/dist/needle-app.js
+++ PagNeedle/ar-mindar/dist/needle-app.js
@@ -53,7 +53,7 @@ if (!customElements.get(componentName)) {
             while(this.basePath.endsWith('/')) {
                 this.basePath = this.basePath.slice(0, -1);
             }
-            script.src = this.getAttribute('script-src') || `${this.basePath}/assets/index-077fd2d9.js`;
+            script.src = this.getAttribute('script-src') || `${this.basePath}/assets/index-a0737857.js`;
             this.shadowRoot.appendChild(script);
 
             this.needleEngine = document.createElement('needle-engine');
